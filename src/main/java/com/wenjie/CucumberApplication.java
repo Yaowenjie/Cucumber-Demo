e036a4f7f73a1c5572c5d08dbabf39def53bc594
Yaowenjie <wjyao@thoughtworks.com>
2016-06-12 15:08:56 +0800

    Add init project

---
+++ src/main/java/com/wenjie/CucumberApplication.java
@@ -0,0 +1,12 @@
+package com.wenjie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberApplication.class, args);
+	}
+}
